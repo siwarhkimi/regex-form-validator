b4a17e89135daca7f2fe793c75c938f04f26e897
siwarhkimi <cywarhkimi@gmail.com>
2020-03-28 14:15:14 +0100

    html and css finish

--- validation.js
+++ validation.js
@@ -1,1 +1,22 @@
-// validation script here
+
+/*
+- phoneNumber :
+
+  - must be a valid Tunisia number (13 digit) ex: 0021620200200.
+
+- username :
+
+  - must be alphanumeric and contain 5-12 characters ex: walid123.
+
+- password :
+
+  - must be alphanumeric contain 5-12 characters,must have uppercase and lowercase ex:WAlid123.
+
+- cardNumber :
+
+  - must be numeric (6-digit) between every two digit (-) ex: 12-35-89.
+
+- email
+
+  - must be a valid email
+*/
